1c8b22c4c93a1e639097ac723370bb13537f0ee2
mklano <mklano@gmail.com>
2026-02-14 09:55:11 -0600

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,28 +1,25 @@
-import { defineStackbitConfig } from "@stackbit/types";
-import { GitContentSource } from "@stackbit/cms-git";
-
-export default defineStackbitConfig({
-  stackbitVersion: "~0.6.0",
-  nodeVersion: "18",
-  ssgName: "astro",
-  contentSources: [
-    new GitContentSource({
-      rootPath: "./",
-      contentDirs: ["src/content"],
-      models: [
-        {
-          name: "Page",
-          type: "page",
-          urlPath: "/{slug}",
-          filePath: "src/content/pages/{slug}.md",
-          fields: [
-            { name: "title", type: "string", required: true },
-            { name: "slug", type: "string", required: true },
-            { name: "seoDescription", type: "string" },
-            { name: "body", type: "markdown" }
-          ]
-        }
-      ]
-    })
-  ]
-});
+models: [
+  {
+    name: "Home",
+    type: "page",
+    urlPath: "/",
+    filePath: "src/content/pages/home.md",
+    fields: [
+      { name: "title", type: "string", required: true },
+      { name: "seoDescription", type: "string" },
+      { name: "body", type: "markdown" }
+    ]
+  },
+  {
+    name: "Page",
+    type: "page",
+    urlPath: "/{slug}",
+    filePath: "src/content/pages/{slug}.md",
+    fields: [
+      { name: "title", type: "string", required: true },
+      { name: "slug", type: "string", required: true },
+      { name: "seoDescription", type: "string" },
+      { name: "body", type: "markdown" }
+    ]
+  }
+]
